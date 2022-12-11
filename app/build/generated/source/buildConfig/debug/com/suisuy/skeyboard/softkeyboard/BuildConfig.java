8a149e7fae544db8cd462c87d5425e40405e4958
Your Name <you@example.com>
2022-12-12 02:31:16 +0800

    shuangpin works now! change package name to com.suisuy...

---
+++ app/build/generated/source/buildConfig/debug/com/suisuy/skeyboard/softkeyboard/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.suisuy.skeyboard.softkeyboard;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.suisuy.skeyboard.softkeyboard";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
